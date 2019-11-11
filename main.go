f58dc387e22058bc78b0bb98f5569cb75ada0f6f
fikribasa <flick_irst@rocketmail.com>
2019-11-12 00:13:03 +0700

    dynamic input

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func countHandler(){
-	inputString:= "aaoms"
+func countHandler() (vowel int, conso int) {
+	fmt.Println("Go String Vowel-Consonant Counter")
+	inputString := readLine("Input Word to Count: ")
 
 	fmt.Println(inputString)
-	vowel:=0
-	conso:=0
+	vowel = 0
+	conso = 0
 
 	for _, value := range inputString {
 		switch value {
-			case 'a','e','i','o','u','A','E','I','O','U' :
-				vowel++
-			default:
-				conso++
+		case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+			vowel++
+		default:
+			conso++
 		}
 	}
-
-	fmt.Printf("Huruf mati: %d, Huruf hidup: %d ",conso,vowel)
-
+	fmt.Printf("Huruf mati: %d, Huruf hidup: %d ", conso, vowel)
+	return
 }
 
 func main() {
 	countHandler()
+
+}
+
+func readLine(word string) string {
+	fmt.Print(word)
+	var input string
+	fmt.Scanln(&input)
+	return input
 }
